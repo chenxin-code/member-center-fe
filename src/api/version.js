c1dafeba0c3f2c22e2b68e9e909d03ee01e0124a
steve zhang <871506263@qq.com>
2020-12-07 17:43:07 +0800

    clear

--- src/api/version.js
+++
@@ -1,7 +0,0 @@
-import { fetchApi } from '@/utils/ajax'
-import URL from './urlConfig'
-
-// APP版本列表
-export const getVersionList = data => fetchApi(URL.VERSION, data, "GET");
-// APP版本发布
-export const addVersion = data => fetchApi(URL.VERSION_ADD, data, "POST");
--- src/store/getters.js
+++
@@ -1,5 +0,0 @@
-const getters = {
-    username: state => state.user.username,
-    useravatar: state => state.user.useravatar,
-}
-export default getters
