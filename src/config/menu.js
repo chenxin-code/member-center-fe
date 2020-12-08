f6ac1e9ceb663eeb07b4e18f783830975c65ea36
steve zhang <871506263@qq.com>
2020-12-08 17:54:57 +0800

    提交代码于: 2020-12-08 17:54:57

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,18 +1,42 @@
 export default [
   {
-    title: "会员列表",
-    authKeys: ["app_owner_archive"],
-    icon: require("@/assets/img/menu/icon_zhuhudangan.png"),
-    menuKey: "resident",
-    group: "zhda",
-    path: "/resident",
+    title: '会员列表',
+    authKeys: ['app_owner_archive'],
+    icon: require('@/assets/img/menu/icon_zhuhudangan.png'),
+    menuKey: 'resident',
+    group: 'zhda',
+    path: '/resident'
   },
   {
-    title: "会员标签",
-    authKeys: ["app_owner_archive"],
-    icon: require("@/assets/img/menu/icon_zhuhudangan.png"),
-    menuKey: "residentLabel",
-    group: "zhda",
-    path: "/residentLabel",
+    title: '会员标签',
+    authKeys: ['app_owner_archive'],
+    icon: require('@/assets/img/menu/icon_zhuhudangan.png'),
+    menuKey: 'residentLabel',
+    group: 'zhda',
+    path: '/residentLabel'
+  },
+  {
+    title: '404页面',
+    authKeys: ['app_owner_archive'],
+    icon: require('@/assets/img/menu/icon_zhuhudangan.png'),
+    menuKey: 'pageError',
+    group: 'zhda',
+    path: '/404'
+  },
+  {
+    title: '403页面',
+    authKeys: ['app_owner_archive'],
+    icon: require('@/assets/img/menu/icon_zhuhudangan.png'),
+    menuKey: 'pageError',
+    group: 'zhda',
+    path: '/403'
+  },
+  {
+    title: '500页面',
+    authKeys: ['app_owner_archive'],
+    icon: require('@/assets/img/menu/icon_zhuhudangan.png'),
+    menuKey: 'pageError',
+    group: 'zhda',
+    path: '/500'
   }
 ];
