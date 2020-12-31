1114ff8b88481f9c78aa42dce299df149517f40e
steve zhang <871506263@qq.com>
2020-12-31 10:09:08 +0800

    提交代码于: 2020-12-31 10:09:08

--- src/api/card.js
+++
@@ -1,6 +0,0 @@
-import { fetchApi } from '@/utils/ajax'
-import URL from './urlConfig'
-
-export const getCardList = data => fetchApi(URL.GET_CARDLIST, data, 'GET');
-
-export const getCardDetail = data => fetchApi(URL.GET_CARDDETAIL, data, 'GET');
