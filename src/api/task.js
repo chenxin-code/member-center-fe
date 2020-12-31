776f26a66af3de5c3a92ce0ea27e33cbade33ca5
steve zhang <871506263@qq.com>
2020-12-31 17:05:17 +0800

    提交代码于: 2020-12-31 17:05:17

--- src/api/task.js
+++
@@ -1,8 +0,0 @@
-import { fetchApi } from '@/utils/ajax'
-import URL from './urlConfig'
-
-export const getTaskList = data => fetchApi(URL.GET_TASKlIST, data, "GET")
-
-export const getTaskSource = () => fetchApi(URL.GET_TASKSOURCE, {}, 'GET')
-
-export const getTaskDetail = data => fetchApi(URL.GET_TASKDETAIL, data, 'GET')
