6d228a442cfe756680bac08d08a08b23d779f724
steve zhang <871506263@qq.com>
2021-01-06 10:07:51 +0800

    提交代码于: 2021-01-06 10:07:50

---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import router from '../router';
+import user from './module/user';
+import app from './module/app';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    paramMap: {}
+  },
+  getters: {
+    username: state => state.user.username,
+    useravatar: state => state.user.useravatar
+  },
+  mutations: {
+    REFRESHPARAM(state, paramKV) {
+      //mutation，应该能看懂做的操作吧？
+      Vue.set(state.paramMap, paramKV.key, paramKV.value);
+    }
+  },
+  actions: {
+    // 返回
+    FALLBACK() {
+      router.go(-1);
+    }
+  },
+  modules: {
+    user,
+    app
+  }
+});
+
+export default store;
