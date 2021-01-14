c1983e313b89eca182ecdd518dda38b2f46a9972
steve zhang <871506263@qq.com>
2021-01-14 16:44:09 +0800

    提交代码于: 2021-01-14 16:44:09

---
+++ src/pages/coupons/cancel/list.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>卡券核销记录</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/pages/coupons/claim/list.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>卡券领取记录</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped></style>
