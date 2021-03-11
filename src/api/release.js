47ae90b5677b8f61e9bd9e3ddf65afb2dea03fc4
steve zhang <871506263@qq.com>
2021-03-11 17:07:34 +0800

    提交代码于: 2021-03-11 17:07:34

--- src/api/release.js
+++ src/api/release.js
@@ -1,13 +1,16 @@
-import { fetchApi, download } from '@/utils/ajax'
-import URL from './urlConfig'
+import { fetchApi, download } from '@/utils/ajax';
+import URL from './urlConfig';
 let tokenStr = 'Bearer ' + localStorage.getItem('SD_ACCESS_TOKEN');
+console.log('tokenStr 666:>> ', tokenStr);
 
-export const getReleaseList = data => fetchApi(URL.GET_RELEASELIST, data, "GET")
-export const getReleaseDetail = data => fetchApi(URL.GET_RELEASEDETAIL, data, "GET")
-export const getCouponList = data => fetchApi(URL.GET_COUPONLIST, data, "GET")
-export const couponDistribute = data => fetchApi(URL.COUPONDISTRIBUTE, data, "POST", {
-    Authorization: tokenStr,
-    'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
-  },)
+export const getReleaseList = data => fetchApi(URL.GET_RELEASELIST, data, 'GET');
+export const getReleaseDetail = data => fetchApi(URL.GET_RELEASEDETAIL, data, 'GET');
+export const getCouponList = data => fetchApi(URL.GET_COUPONLIST, data, 'GET');
+export const couponDistribute = data => fetchApi(URL.COUPONDISTRIBUTE, data, 'POST');
+// export const couponDistribute = data =>
+//   fetchApi(URL.COUPONDISTRIBUTE, data, 'POST', {
+//     Authorization: tokenStr,
+//     'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
+//   });
 
-export const getMemberLevelList = data => fetchApi(URL.GET_MEMBERLEVELLIST, data, "GET")
+export const getMemberLevelList = data => fetchApi(URL.GET_MEMBERLEVELLIST, data, 'GET');
