37106c3262961431391e6fa7794a6bd147f0b30d
steve zhang <871506263@qq.com>
2021-03-11 18:06:02 +0800

    提交代码于: 2021-03-11 18:06:02

--- src/api/release.js
+++ src/api/release.js
@@ -6,11 +6,11 @@ console.log('tokenStr 666:>> ', tokenStr);
 export const getReleaseList = data => fetchApi(URL.GET_RELEASELIST, data, 'GET');
 export const getReleaseDetail = data => fetchApi(URL.GET_RELEASEDETAIL, data, 'GET');
 export const getCouponList = data => fetchApi(URL.GET_COUPONLIST, data, 'GET');
-export const couponDistribute = data => fetchApi(URL.COUPONDISTRIBUTE, data, 'POST');
-// export const couponDistribute = data =>
-//   fetchApi(URL.COUPONDISTRIBUTE, data, 'POST', {
-//     Authorization: tokenStr,
-//     'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
-//   });
+// export const couponDistribute = data => fetchApi(URL.COUPONDISTRIBUTE, data, 'POST');//好像默认'Content-Type': 'application/json' 也会自动转成 'Content-Type': 'multipart/form-data
+export const couponDistribute = data =>
+  fetchApi(URL.COUPONDISTRIBUTE, data, 'POST', {
+    Authorization: tokenStr,
+    'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>'
+  });
 
 export const getMemberLevelList = data => fetchApi(URL.GET_MEMBERLEVELLIST, data, 'GET');
