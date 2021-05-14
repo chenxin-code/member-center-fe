603aae9e8335578b1077ad691d73e352ac732894
李国团 <1374725370@qq.com>
2021-05-14 18:39:37 +0800

    feat: 会员中心开发

--- src/pages/game/addGame.vue
+++ src/pages/game/addGame.vue
@@ -4,12 +4,12 @@
     <div class="game-add-content">
       <div class="label-content">
         <div class="label-title" style="margin: 0">游戏主题</div>
-        <timesInput v-model="gameTheme"></timesInput>
+        <timesInput v-model="gameTitle"></timesInput>
       </div>
 
       <div class="choose-isActive">
         <div class="label-title">是否可用</div>
-        <a-radio-group v-model="radio" @change="radioChange">
+        <a-radio-group v-model="availableFlage" @change="radioChange">
           <a-radio :value="1">
             启用
           </a-radio>
@@ -25,18 +25,18 @@
 
       <div class="label-content">
         <div class="label-title" style="margin: 0;">参与人数</div>
-        <timesInput v-model="gamePeopleNum"></timesInput>
+        <timesInput v-model="partakeNum"></timesInput>
       </div>
 
       <div class="label-content">
         <div class="label-title" style="margin: 0;">每人可参与次数</div>
-        <timesInput v-model="gamePeopleTimes"></timesInput>
+        <timesInput v-model="luckyDrawLimits"></timesInput>
       </div>
 
       <div class="label-content">
         <div class="label-title">活动说明</div>
         <a-textarea
-          v-model="gameExplain"
+          v-model="activityDesc"
           placeholder="请输入内容"
           :auto-size="{ minRows: 3, maxRows: 5 }"
           style="width: 400px;"
@@ -50,7 +50,11 @@
 
       <div class="label-content">
         <div class="label-title" style="margin: 0;">开奖方式</div>
-        <timesSelect :optionObj="prizeOption" @select-option="selectPrize" placeholder="请选择开奖方式"></timesSelect>
+        <timesSelect
+          :optionObj="prizeOption"
+          @select-option="selectPrize"
+          placeholder="请选择开奖方式"
+        ></timesSelect>
       </div>
 
       <div class="label-content">
@@ -69,16 +73,26 @@
             <div style="margin-bottom: 15px;">背景图片</div>
             <a-upload
               name="avatar"
+              accept="image/jpeg,image/jpg,image/png"
               list-type="picture-card"
-              class="avatar-uploader"
-              :show-upload-list="false"
-              :before-upload="stairBefore"
-              @change="stairChange"
+              :before-upload="() => false"
+              :remove="handleImgRemoveBg"
+              @preview="handlePreview"
+              @change="uploadBg"
+              v-decorator="[
+                'activityCover',
+                { initialValue: activityCover, rules: [{ required: true, message: '图片不能为空' }] }
+              ]"
             >
-              <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-              <div v-else>
-                <a-icon :type="loading ? 'loading' : 'plus'" />
-              </div>
+              <template v-if="!activityBackgroundUrl">
+                <a-icon :type="picUploading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">
+                  上传
+                </div>
+              </template>
+              <!-- <template v-else>
+                <img :src="activityBackgroundUrl" alt="" style="width: 80px;height: 80px;" />
+              </template> -->
             </a-upload>
           </div>
 
@@ -86,16 +100,23 @@
             <div style="margin-bottom: 15px;">消息层背景</div>
             <a-upload
               name="avatar"
+              accept="image/jpeg,image/jpg,image/png"
               list-type="picture-card"
-              class="avatar-uploader"
-              :show-upload-list="false"
-              :before-upload="stairBefore"
-              @change="stairChange"
+              :before-upload="() => false"
+              :remove="handleImgRemoveMessage"
+              @preview="handlePreview"
+              @change="uploadMessage"
+              v-decorator="[
+                'activityCover',
+                { initialValue: activityCover, rules: [{ required: true, message: '图片不能为空' }] }
+              ]"
             >
-              <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-              <div v-else>
-                <a-icon :type="loading ? 'loading' : 'plus'" />
-              </div>
+              <template v-if="!msgUrl">
+                <a-icon :type="picUploading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">
+                  上传
+                </div>
+              </template>
             </a-upload>
           </div>
 
@@ -103,16 +124,47 @@
             <div style="margin-bottom: 15px;">游戏层背景</div>
             <a-upload
               name="avatar"
+              accept="image/jpeg,image/jpg,image/png"
               list-type="picture-card"
-              class="avatar-uploader"
-              :show-upload-list="false"
-              :before-upload="stairBefore"
-              @change="stairChange"
+              :before-upload="() => false"
+              :remove="handleImgRemoveGame"
+              @preview="handlePreview"
+              @change="uploadGame"
+              v-decorator="[
+                'activityCover',
+                { initialValue: activityCover, rules: [{ required: true, message: '图片不能为空' }] }
+              ]"
             >
-              <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-              <div v-else>
-                <a-icon :type="loading ? 'loading' : 'plus'" />
-              </div>
+              <template v-if="!gameUrl">
+                <a-icon :type="picUploading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">
+                  上传
+                </div>
+              </template>
+            </a-upload>
+          </div>
+
+          <div class="stair">
+            <div style="margin-bottom: 15px;">弹框背景</div>
+            <a-upload
+              name="avatar"
+              accept="image/jpeg,image/jpg,image/png"
+              list-type="picture-card"
+              :before-upload="() => false"
+              :remove="handleImgRemoveAlert"
+              @preview="handlePreview"
+              @change="uploadAlert"
+              v-decorator="[
+                'activityCover',
+                { initialValue: activityCover, rules: [{ required: true, message: '图片不能为空' }] }
+              ]"
+            >
+              <template v-if="!popFrameUrl">
+                <a-icon :type="picUploading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">
+                  上传
+                </div>
+              </template>
             </a-upload>
           </div>
         </div>
@@ -127,6 +179,9 @@
 </template>
 
 <script>
+import { getFormatDate } from '@/utils/util';
+import { GANE_SAVE_GAME } from '@/api/game';
+import { updateImage } from '@/api/member';
 import timesInput from './component/form-input';
 import timesSelect from './component/form-select';
 export default {
@@ -136,57 +191,151 @@ export default {
   },
   data() {
     return {
-      gameTheme: '',
-      radio: 1,
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('SD_ACCESS_TOKEN'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      picUploading: false,
+      activityCover: '',
+
+      gameTitle: '',
+      availableFlage: 1,
+      validityStartTime: '', // 活动结束时间
+      validityEndTime: '', //活动开始时间
+      partakeNum: '', // 参与人数
+      luckyDrawLimits: '', //每人可参与次数
+      activityDesc: '', //活动说明
+      noticeType: '', // 通知方式
+      lotteryType: '', // 开奖方式
+      activityType: '', // 活动方式
+      msgUrl: '', //消息层底图
+      activityBackgroundUrl: '', //上传抽奖活动背景
+      gameUrl: '', // 游戏层底图
+      popFrameUrl: '', // 弹窗背景Url
+
       gamePeopleNum: '',
       gamePeopleTimes: '',
       gameExplain: '',
-      informOption: [
-        { name: '消息通知', value: 'sudoku' },
-        { name: '短信通知', value: 'turntable' }
-      ],
+      informOption: [{ name: '消息推送', value: 1 }],
       informType: '', //通知方式
-      prizeOption: [
-        { name: '消息通知', value: 'sudoku' },
-        { name: '短信通知', value: 'turntable' }
+      // activityOption
+      // prizeOption
+      activityOption: [
+        { name: '幸运转盘', value: 1 },
+        { name: '砸金蛋', value: 2 },
+        { name: '九宫格', value: 3 },
       ],
       prizeType: '', //开奖方式
-      activityOption: [
-        { name: '消息通知', value: 'sudoku' },
-        { name: '短信通知', value: 'turntable' }
+      prizeOption: [
+        { name: '立即开奖', value: 1 },
+        { name: '非立即开奖', value: 2 }
       ],
-      activityType: '', //活动方式
       loading: false,
       imageUrl: ''
     };
   },
   methods: {
     radioChange(e) {
-      this.radio = e.target.value;
+      this.availableFlage = e.target.value;
     },
+    // 日期选择
     changeDate(val) {
-      console.log('val', val);
+      this.validityStartTime = getFormatDate(val[0]._d, 'yyyy-mm-dd MM:mm:ss');
+      this.validityEndTime = getFormatDate(val[1]._d, 'yyyy-mm-dd MM:mm:ss');
     },
     selectInform(value) {
-      this.informType = value;
+      this.noticeType = value;
     },
     selectPrize(value) {
-      this.prizeType = value;
+      this.lotteryType = value;
     },
     selectActivity(value) {
       this.activityType = value;
     },
-    stairBefore() {},
-    stairChange() {},
+    stairBefore(info) {
+      console.log('>>>stairBefore>>>', info);
+    },
+    handleImgRemoveBg() {
+      this.activityBackgroundUrl = '';
+    },
+    handleImgRemoveMessage() {
+      this.msgUrl = '';
+    },
+    handleImgRemoveGame() {
+      this.gameUrl = '';
+    },
+    handleImgRemoveAlert() {
+      this.popFrameUrl = '';
+    },
+    // 上传背景图片
+    uploadBg({ fileList = [] } = {}) {
+      this.addPic(fileList, 'activityBackgroundUrl');
+    },
+    // 上传消息背景图片
+    uploadMessage({ fileList = [] } = {}) {
+      this.addPic(fileList, 'msgUrl');
+    },
+    // 上传游戏层背景图片
+    uploadGame({ fileList = [] } = {}) {
+      this.addPic(fileList, 'gameUrl');
+    },
+    // 上传弹框背景图片
+    uploadAlert({ fileList = [] } = {}) {
+      this.addPic(fileList, 'popFrameUrl');
+    },
+    addPic(fileList, flag) {
+      if (fileList.length > 0) {
+        const isJpgOrPng = fileList[0].type === 'image/jpeg' || fileList[0].type === 'image/png';
+        if (!isJpgOrPng) {
+          this.$message.error('图片格式错误，请重新上传');
+        } else {
+          this.picUploading = true;
+          const formData = new FormData();
+          fileList.forEach(file => {
+            formData.append('file', file.originFileObj);
+          });
+          formData.append('programCode', 'sys-member-center');
+          updateImage(formData)
+            .finally(() => {
+              this.picUploading = false;
+            })
+            .then(res => {
+              if (res.code === 200) {
+                this[flag] = res.data;
+              }
+            });
+        }
+      }
+    },
+    handlePreview(file) {
+      console.log('>>stairPreview>>>', file);
+    },
     cancel() {
       this.$router.go(-1);
     },
     submit() {
       let params = {
-        gameTitle: this.gameTheme,
-        availableFlage: this.radio,
-        validityStartTime: ''
+        gameTitle: this.gameTitle,
+        availableFlage: this.availableFlage,
+        validityStartTime: this.validityStartTime,
+        validityEndTime: this.validityEndTime,
+        partakeNum: this.partakeNum,
+        luckyDrawLimits: this.luckyDrawLimits,
+        activityDesc: this.activityDesc,
+        noticeType: this.noticeType,
+        lotteryType: this.lotteryType,
+        activityType: this.activityType,
+        activityBackgroundUrl: this.activityBackgroundUrl,
+        popFrameUrl: this.popFrameUrl,
+        gameUrl: this.gameUrl,
+        msgUrl: this.msgUrl
       };
+      console.log('>>>>>>>>>', params);
+      GANE_SAVE_GAME(params).then(res => {
+        if (res.code == 200) {
+          console.log('res', res);
+        }
+      });
     }
   }
 };
